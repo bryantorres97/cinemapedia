445371d0ab38a5ab83bcc6b2bc242266f8ed8146
Bryan Torres <bryantorresckp@gmail.com>
2024-12-06 12:57:43 -0500

    feat: Implement movie loading in HomeScreen with pagination support

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:cinemapedia_app/presentation/providers/movies/movies_providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
@@ -8,7 +10,41 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Placeholder(),
+      body: _HomeView(),
     );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(moviesNotifierProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final movies = ref.watch(moviesNotifierProvider);
+    return ListView.builder(
+        itemCount: movies.length + 1,
+        itemBuilder: (context, index) {
+          if (index >= movies.length) {
+            ref.read(moviesNotifierProvider.notifier).loadNextPage();
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          final movie = movies[index];
+          return ListTile(
+            title: Text(movie.title),
+            subtitle: Text(movie.overview),
+          );
+        });
   }
 }
